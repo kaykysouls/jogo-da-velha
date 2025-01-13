b55bbeb91bc204b90885805b291ecad60c288882
kaykysouls <123007608+kaykysouls@users.noreply.github.com>
2025-01-13 11:45:04 -0300

    alteração no nome do jogador

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        var jogador = new Jogador("Amor da Evelyn");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite seu nome de jogador: ");
+        String nome = input.nextLine();
+
+        var jogador = new Jogador(nome);
         var palavra = new Palavra();
         var jogo = new Jogo(palavra, jogador);
 
         jogo.iniciarJogo();
+
+        input.close();
     }
 }
